9c4f7e639c909c50458be5abe19d6d159562533f
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-04-09 04:21:21 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -9,10 +10,26 @@ app.use(cors());
 app.use(express.json())
 
 
-app.get('/', (req, res)=>{
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.te1qmpf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+          try{
+
+          }
+          finally{
+
+          }
+}
+
+run().catch(console.dir)
+
+
+app.get('/', (req, res) => {
           res.send('John is rumming for Ema')
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
           console.log('Ema-John is running');
 })
