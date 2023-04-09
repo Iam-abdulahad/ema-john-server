9acc604b49dc8d02641d1c94232cc58b2f915dc1
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-04-10 05:44:45 +0600

    introducs to pagination system

--- index.js
+++ index.js
@@ -15,19 +15,50 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.te1qmpf.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-          try{
+async function run() {
+          try {
                     client.connect();
                     const productCollection = client.db('EmaJohn').collection('product');
 
-                    app.get('/product', async(req, res) =>{
+                    app.get('/product', async (req, res) => {
+                              console.log('query', req.query);
+                              const page = parseInt(req.query.page);
+                              const size = parseInt(req.query.size);
+
                               const query = {};
                               const cursor = productCollection.find(query);
+                              let products;
+                              if (page || size) {
+                                        // 0 --> skip: 0 get: 0-10(10): 
+                                        // 1 --> skip: 1*10 get: 11-20(10):
+                                        // 2 --> skip: 2*10 get: 21-30 (10):
+                                        // 3 --> skip: 3*10 get: 21-30 (10):
+                                        products = await cursor.skip(page * size).limit(size).toArray();
+                              }
+                              else {
+                                        products = await cursor.toArray();
+                              }
+
+                              res.send(products);
+                    });
+
+                    app.get('/productCount', async (req, res) => {
+                              const count = await productCollection.estimatedDocumentCount();
+                              res.send({ count });
+                    });
+
+                    // use post to get products by ids
+                    app.post('/productByKeys', async (req, res) => {
+                              const keys = req.body;
+                              const ids = keys.map(id => ObjectId(id));
+                              const query = { _id: { $in: ids } }
+                              const cursor = productCollection.find(query);
                               const products = await cursor.toArray();
+                              console.log(keys);
                               res.send(products);
                     })
           }
-          finally{
+          finally {
 
           }
 }
@@ -36,7 +67,7 @@ run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
-          res.send('John is rumming for Ema')
+          res.send('John is runncmding for Ema')
 })
 
 app.listen(port, () => {
